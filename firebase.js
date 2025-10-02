21a895fcd4cee605f845bc6b6741c22ed97134f1
DeberesFisica <cs-hernandez@outlook.com>
2025-10-02 17:01:05 -0500

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,4 +1,7 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-firestore.js";
+
 const firebaseConfig = {
   apiKey: "AIzaSyBol96sCZ35ae3uVbACz6fMC70KysNlku8",
   authDomain: "deberes-tecnologia.firebaseapp.com",
@@ -8,4 +11,11 @@ const firebaseConfig = {
   messagingSenderId: "320356541909",
   appId: "1:320356541909:web:68e047e074b43d1a89895c",
   measurementId: "G-FYZGZ52YQT"
+
 };
+
+// Inicializa Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export { db };
