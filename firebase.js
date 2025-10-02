8f36b40405c05893e1f2f6dc08a5a3df535d5b82
DeberesFisica <cs-hernandez@outlook.com>
2025-10-02 16:57:41 -0500

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBol96sCZ35ae3uVbACz6fMC70KysNlku8",
+  authDomain: "deberes-tecnologia.firebaseapp.com",
+  databaseURL: "https://deberes-tecnologia-default-rtdb.firebaseio.com",
+  projectId: "deberes-tecnologia",
+  storageBucket: "deberes-tecnologia.firebasestorage.app",
+  messagingSenderId: "320356541909",
+  appId: "1:320356541909:web:68e047e074b43d1a89895c",
+  measurementId: "G-FYZGZ52YQT"
+};
